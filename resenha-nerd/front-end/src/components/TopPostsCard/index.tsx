fe68274d078fcbe75d72bf635cee3b4166f0be8c
vinnidias <vinicius.3991@gmail.com>
2024-03-13 11:40:01 -0300

    ajustes de responsividade

--- resenha-nerd/front-end/src/components/TopPostsCard/index.tsx
+++ resenha-nerd/front-end/src/components/TopPostsCard/index.tsx
@@ -19,18 +19,18 @@ export default function TopPostCard(props: IProps) {
     year: "numeric",
   });
   return (
-    <div className="flex w-400 h-100 gap-4">
+    <div className="flex flex-row w-[100%] md:w-400 h-100 gap-4 p-2 items-center">
       <Image
         src={bannerUrl}
         alt="banner"
         className="w-[215px] h-[120px] aspect-auto rounded-md"
       />
-      <div className="flex flex-col gap-4 w-[70%] max-w-[80%]">
-        <p className="font-bold text-blue-400">{category}</p>
-        <p className="font-semibold 2xl:text-xl text-lg">{title}</p>
-        <div className="flex justify-between pr-4 border-t border-blue-300 pt-1">
-          <p className="text-sm">🤓 {author} </p>
-          <p className="text-sm">{date.format(createdAt)}</p>
+      <div className="flex flex-col gap-1 md:gap-4 w-[70%] max-w-[80%]">
+        <p className="font-bold text-blue-400 text-sm">{category}</p>
+        <p className="font-semibold 2xl:text-xl md:text-lg text-sm">{title}</p>
+        <div className="flex justify-between md:pr-4 border-t border-blue-300 pt-1">
+          <p className="text-xs md:text-sm">🤓 {author} </p>
+          <p className="text-xs md:text-sm">{date.format(createdAt)}</p>
         </div>
       </div>
     </div>
