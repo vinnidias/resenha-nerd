a5c4b5384e8faaae76e97452b95e9b0bae20de9c
vinnidias <vinicius.3991@gmail.com>
2024-03-14 12:19:09 -0300

    adicionando footer

--- resenha-nerd/front-end/src/app/layout.tsx
+++ resenha-nerd/front-end/src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main>
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
