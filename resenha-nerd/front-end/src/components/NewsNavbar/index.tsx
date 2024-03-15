98ef4fbb96a16a3fc3d08cf0a4e28a301a2b659b
vinnidias <vinicius.3991@gmail.com>
2024-03-15 00:06:05 -0300

    barra de navegação de noticias

---
+++ resenha-nerd/front-end/src/components/NewsNavbar/index.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function NewsNavbar() {
+  const categorys = [
+    {
+      name: "Games",
+      link: "/",
+    },
+    {
+      name: "Tecnologia",
+      link: "/",
+    },
+    {
+      name: "Música",
+      link: "/",
+    },
+    {
+      name: "Series",
+      link: "/",
+    },
+    {
+      name: "Cinema",
+      link: "/",
+    },
+    {
+      name: "Animes",
+      link: "/",
+    },
+  ];
+
+  return (
+    <nav className="flex w-full px-8 pt-28 gap-24 h-48 items-center bg-gradient-to-r from-slate-600 to-slate-400 shadow-md">
+      <h1 className="text-2xl text-white font-bold">📰 Notícias</h1>
+      <ul className="flex w-[50%] ml-[14%] items-center pt-4 justify-between gap-6 h-20">
+        {categorys.map((category, index) => (
+          <li key={index} className="text-sm pt-4 ease-in-out duration-300 hover:border-b-4 hover:border-blue-400 h-full">
+            <Link href={category.link} className="text-white hover:text-white">{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
