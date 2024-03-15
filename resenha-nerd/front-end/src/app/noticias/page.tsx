a517d68484709512d69670a013e02c2804b2ea7a
vinnidias <vinicius.3991@gmail.com>
2024-03-14 23:58:34 -0300

    iniciando página de notícias

---
+++ resenha-nerd/front-end/src/app/noticias/page.tsx
@@ -0,0 +1,9 @@
+import NewsNavbar from "@/components/NewsNavbar";
+
+export default function News() {
+  return (
+    <div className="mt-[-8rem] pt-[3.5em] h-[80vh]">
+      <NewsNavbar />
+    </div>
+  );
+}
