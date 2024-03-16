da1795b17238d305d5d3d870ffdff3e98c2c7599
vinnidias <vinicius.3991@gmail.com>
2024-03-16 12:16:09 -0300

    feed de noticias

--- resenha-nerd/front-end/src/app/noticias/page.tsx
+++ resenha-nerd/front-end/src/app/noticias/page.tsx
@@ -1,9 +1,27 @@
 import NewsNavbar from "@/components/NewsNavbar";
+import TopPostCard from "@/components/TopPostsCard";
+import { newsList } from "@/mockDatas/newsData";
 
 export default function News() {
   return (
-    <div className="mt-[-8rem] pt-[3.5em] h-[80vh]">
+    <div className="flex flex-col mt-[-8rem] pt-[3.5em] min-h-[80vh]">
       <NewsNavbar />
+
+      <div className="flex flex-col w-[70%] gap-20 py-12 pl-72 border-r border-blue-300">
+        <h2 className="text-2xl font-bold">TODAS AS NOTÍCIAS EM UM SÓ LUGAR</h2>
+        {newsList.map((item, index) => (
+          <TopPostCard
+            category={item.category}
+            author={item.author}
+            createdAt={item.cratedAt}
+            bannerUrl={item.bannerUrl}
+            title={item.title}
+            subtitle={item.subtitle}
+            post={item.post}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
