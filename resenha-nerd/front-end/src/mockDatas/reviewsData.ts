c58b203108c320394983790fb9b919497fa8e9c8
vinnidias <vinicius.3991@gmail.com>
2024-03-16 11:37:08 -0300

    mock dos dados para noticias e resenhas

---
+++ resenha-nerd/front-end/src/mockDatas/reviewsData.ts
@@ -0,0 +1,58 @@
+const today = new Date();
+
+export const reviewsList = [
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+  {
+    author: "Vinni Dias",
+    category: "Games",
+    title: "GTA 6 no site da Rockstar",
+    subtitle:
+      "Finalmente GTA 6  no site, vamos discutir sobre a relevância e como a Rockstar está lidando com GTA após primeiro anúncio",
+    createdAt: today,
+    relevance: "90%",
+  },
+];
