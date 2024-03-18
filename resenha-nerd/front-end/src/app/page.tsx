e7919677f1350c2f5e34f83d4633f85aea2de6fc
vinnidias <vinicius.3991@gmail.com>
2024-03-18 15:17:30 -0300

    abstraindo sessao para server component isolado

--- resenha-nerd/front-end/src/app/page.tsx
+++ resenha-nerd/front-end/src/app/page.tsx
@@ -4,6 +4,8 @@ import TopReviewCard from "@/components/TopReviewCard";
 
 import { newsList } from "@/mockDatas/newsData";
 import { reviewsList } from "@/mockDatas/reviewsData";
+import Image from "next/image";
+import RecentNewsSection from "@/components/RecentNewsSection";
 
 export default function Home() {
   return (
@@ -17,37 +19,7 @@ export default function Home() {
         subtitle={newsList[0].subtitle}
         post={newsList[0].post}
       />
-      <section className="flex flex-col md:flex-row w-[100%] px-2 md:px-[6em] 2xl:px-[12em] border-t border-blue-300">
-        <div className="flex flex-col w-[100%] md:w-[60%] py-[3em] pr-2 md:pr-8 gap-24">
-          <h2 className="text-2xl font-bold">📰 Notícias Recentes</h2>
-          {newsList.map((item, index) => (
-            <TopPostCard
-              category={item.category}
-              author={item.author}
-              createdAt={item.cratedAt}
-              bannerUrl={item.bannerUrl}
-              title={item.title}
-              subtitle={item.subtitle}
-              post={item.post}
-              key={index}
-            />
-          ))}
-        </div>
-        <div className="flex flex-col w-[100%] md:w-[40%] py-[3em] pl-8 md:border-l border-blue-300 gap-24">
-          <h2 className="text-2xl font-bold">✏️ Resenhas em Destaque</h2>
-          {reviewsList.map((item, index) => (
-            <TopReviewCard
-              author={item.author}
-              category={item.category}
-              title={item.title}
-              subtitle={item.subtitle}
-              createdAt={item.createdAt}
-              relevance={item.relevance}
-              key={index}
-            />
-          ))}
-        </div>
-      </section>
+      <RecentNewsSection />
     </main>
   );
 }
