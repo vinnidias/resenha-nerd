4edff41d12594838340d49491fc3f12117e3d0c5
vinnidias <vinicius.3991@gmail.com>
2024-03-20 04:07:34 -0300

    avancando na pagina de noticia

--- resenha-nerd/front-end/src/app/noticia/[id]/page.tsx
+++ resenha-nerd/front-end/src/app/noticia/[id]/page.tsx
@@ -1,25 +1,71 @@
+import Iframe from "@/components/Iframe";
+import Image from "next/image";
+
 interface IParams {
   params: { id: string };
 }
-interface IResponseData{
+interface IResponseData {
   id: string;
   author: string;
   title: string;
   subtitle: string;
   category: string;
   content: string;
+  end_text?: string;
   image?: string;
+  image_credits?: string;
+  link?: string;
   created_at: Date;
   updated_at?: Date;
 }
 
 export default async function Noticia({ params }: IParams) {
   const { id } = params;
-  const res = await fetch(`http://localhost:3333/newsbyid?id=${id}`);
-  const data = await res.json();
+  const res = await fetch(`http://localhost:3333/newsbyid?id=${id}`, {
+    cache: "no-store",
+  });
+  const data: IResponseData = await res.json();
 
-  console.log("response: ", data);
-  console.log("id: ", id);
+  const title = data.title.toUpperCase();
+  const subtitle = data.subtitle;
+  const content = data.content;
+  const imagePath = `data:image/jpeg;base64,${data.image}`;
+  const category = data.category;
+  const endText = data.end_text;
+  const link = data.link;
+  const imageCredits = data.image_credits;
 
-  return <h1>Notícia</h1>;
+  console.log(link);
+  return (
+    <div className="flex flex-col min-w-screen min-h-screen">
+      <div className="flex flex-col justify-start 2xl:gap-8 mt-24 h-screen md:mt-4 gap-2">
+        <Image
+          src={imagePath || ""}
+          alt="Banner da notícia"
+          width={460}
+          height={300}
+          className="min-w-[50%] self-center z-0 rounded-md"
+        />
+        <div className="flex flex-col gap-0 w-full lg:w-[70%] self-center px-2 md:mt-0 md:px-16 md:justify-center">
+          <p className="text-blue-500 font-bold 2xl:text-lg lg:text-md text-center lg:text-start">
+            {category}
+          </p>
+          <h1 className="text-2xl 2xl:text-3xl font-bold mb-8 mt-8 text-center md:text-start">
+            {title}
+          </h1>
+          <h2 className="text-center text-md lg:text-xl">{subtitle}</h2>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-20 px-4 w-[full] lg:w-[50%] lg:ml-[15%] py-8 lg:pl-2 lg:pr-4 2xl:pl-12 lg:border-r lg:border-blue-300">
+        <p className="text-lg lg:text-xl tracking-wide leading-9">{content}</p>
+        <Iframe link={link || ""} />
+        <p className="text-lg lg:text-xl tracking-wide leading-9">
+          {endText}
+        </p>
+      </div>
+
+      <div className="flex w-full justify-center p-8"></div>
+    </div>
+  );
 }
