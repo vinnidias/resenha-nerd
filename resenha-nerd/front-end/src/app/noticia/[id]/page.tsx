55b5d24e04555f82541c2b79debad600cf8e8045
vinnidias <vinicius.3991@gmail.com>
2024-03-20 22:23:18 -0300

    adicionando rodape do autor na noticia

--- resenha-nerd/front-end/src/app/noticia/[id]/page.tsx
+++ resenha-nerd/front-end/src/app/noticia/[id]/page.tsx
@@ -1,5 +1,9 @@
-import Iframe from "@/components/Iframe";
 import Image from "next/image";
+import Link from "next/link";
+
+import { FaInstagram, FaGithub } from "react-icons/fa6";
+
+import Iframe from "@/components/Iframe";
 
 interface IParams {
   params: { id: string };
@@ -33,7 +37,17 @@ export default async function Noticia({ params }: IParams) {
   const category = data.category;
   const endText = data.end_text;
   const link = data.link;
+  const author = data.author;
   const imageCredits = data.image_credits;
+  const formatedDate = new Intl.DateTimeFormat("pt-br", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const date = formatedDate.format(new Date(data.created_at));
 
   console.log(link);
   return (
@@ -44,25 +58,48 @@ export default async function Noticia({ params }: IParams) {
           alt="Banner da notícia"
           width={460}
           height={300}
-          className="min-w-[50%] self-center z-0 rounded-md"
+          className="min-w-[50%] self-center 2xl:mt-16 z-0 rounded-md lg:min-w-[45%]"
         />
         <div className="flex flex-col gap-0 w-full lg:w-[70%] self-center px-2 md:mt-0 md:px-16 md:justify-center">
           <p className="text-blue-500 font-bold 2xl:text-lg lg:text-md text-center lg:text-start">
             {category}
           </p>
-          <h1 className="text-2xl 2xl:text-3xl font-bold mb-8 mt-8 text-center md:text-start">
+          <h1 className="text-2xl lg:text-xl lg:mt-[-0.15em] 2xl:text-3xl font-bold mb-8 mt-8 text-center md:text-start">
             {title}
           </h1>
-          <h2 className="text-center text-md lg:text-xl">{subtitle}</h2>
+          <h2 className="text-md lg:text-lg lg:mt-[-1em] 2xl:text-xl">{subtitle}</h2>
+        </div>
+        <div className="flex w-full px-2 self-center justify-between items-center mt-16 lg:mt-0 lg:items-end h-20 lg:px-2 md:px-16 lg:w-[70%] lg:gap-40 border-t border-blue-300">
+          <div className="flex gap-4 w-full">
+            <p className="text-md lg:text-md lg:px-8 2xl:text-lg font-bold">🤓 {author}</p>
+            <p className="text-md lg:text-lg font-[500]">{date}</p>
+          </div>
+
+          <div className="flex gap-6 pb-1 lg:pr-8 2xl:pr-40 items-center">
+            <a
+              target="blank"
+              href={"https://www.instagram.com/vinni.diass/"}
+              className="text-3xl text-center text-blue-400"
+            >
+              {" "}
+              <FaInstagram />
+            </a>
+            <a
+              target="blank"
+              href={"https://github.com/vinnidias"}
+              className="text-3xl text-center text-blue-400 "
+            >
+              {" "}
+              <FaGithub />
+            </a>
+          </div>
         </div>
       </div>
 
       <div className="flex flex-col gap-20 px-4 w-[full] lg:w-[50%] lg:ml-[15%] py-8 lg:pl-2 lg:pr-4 2xl:pl-12 lg:border-r lg:border-blue-300">
         <p className="text-lg lg:text-xl tracking-wide leading-9">{content}</p>
         <Iframe link={link || ""} />
-        <p className="text-lg lg:text-xl tracking-wide leading-9">
-          {endText}
-        </p>
+        <p className="text-lg lg:text-xl tracking-wide leading-9">{endText}</p>
       </div>
 
       <div className="flex w-full justify-center p-8"></div>
