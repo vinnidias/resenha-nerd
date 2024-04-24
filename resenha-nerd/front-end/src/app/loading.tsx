2f99519193b566023825bc3af8c60e6bd8c3fcbe
vinnidias <vinicius.3991@gmail.com>
2024-04-24 02:25:24 -0300

    paginas genericas de erro e loading

--- resenha-nerd/front-end/src/app/loading.tsx
+++ resenha-nerd/front-end/src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading(){
   return(
-    <div className="flex w-screen h-screen items-center justify-center">
+    <div className="flex w-full h-screen items-center justify-center">
       <p>Carregando...</p>
     </div>
   )
